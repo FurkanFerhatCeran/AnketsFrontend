cfca656f80bbd07026d64f55e2c3a4a88995670b
Aysel SÜNDÜK <119888239+aysel-sunduk@users.noreply.github.com>
2025-08-16 14:42:48 +0300

    arayüz düzenlemesi ve not found sayfası

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { AuthGuard } from './guards/auth.guard';
+import { NotFoundComponent } from './pages/NotFound/not-found.component'; // Doğru yolu kontrol et ve düzelt
 import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
+  // Uygulamanın kök dizinini login sayfasına yönlendir.
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, 
+
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { 
@@ -17,6 +21,8 @@ export const routes: Routes = [
       { path: '', redirectTo: 'profile', pathMatch: 'full' }
     ]
   },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: '**', redirectTo: '/dashboard' }
+
+  // ** En sona eklenmeli! **
+  // Geçersiz bir URL girildiğinde NotFoundComponent'e yönlendir
+  { path: '**', component: NotFoundComponent }
 ];
---
+++ src/app/pages/NotFound/not-found.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common'; // Gerekli modül
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router'; // Gerekli modül
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class NotFoundComponent { }
